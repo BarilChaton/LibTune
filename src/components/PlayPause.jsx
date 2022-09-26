7db6c084c35bc51ed1411485fed5306684274b6d
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-09-26 21:28:12 +0200

    music player works..

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,17 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { CgPlayButtonO, CgPlayPauseO } from "react-icons/cg";
+
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => (isPlaying && activeSong?.title === song.title ? (
+  <CgPlayPauseO 
+    size={100}
+    className='text-white'
+    onClick={handlePause}
+  />
+) : (
+  <CgPlayButtonO 
+    size={100}
+    className='text-white'
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,17 +4,45 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
-  const activeSong = 'Test';
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
   return (
-  <div className="flex flex-col w-[250px] p-4 bg-gradient-to-tl from-yellow-100 to-blue-300 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer outline-none shadow-md">
-    <div className='relative w-full h-56 group'>
-      <div className={`absolute inset-0 justify-center items-center bg-blue-600 bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-blue-600 bg-opacity-70' : 'hidden'}`}>
-        <PlayPause />
+    <div className="flex flex-col w-[250px] p-4 bg-gradient-to-tl from-yellow-100 to-blue-300 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer outline-none shadow-md">
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-blue-600 bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-blue-600 bg-opacity-70' : 'hidden'}`}>
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
+        </div>
+        <img src={song.images?.coverart} alt="song_img" />
       </div>
-      <img src={song.images?.coverart} alt="song_img" />
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-black truncate">
+          <Link to={`/songs/${song?.key}`}>
+            {song.title}
+          </Link>
+        </p>
+        <p className="text-sm truncate text-black mt-1">
+          <Link to={song.artist ? `/artists/${song?.artist[0]?.adamid}` : '/top-artists'}>
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default SongCard;
