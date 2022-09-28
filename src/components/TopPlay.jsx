1721d1ada05b72bb1063380575b017a5b977e44f
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-09-28 09:31:59 +0200

    genres working, top artist show before top songs

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -58,26 +58,6 @@ const TopPlay = () => {
 
   return (
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col sm:mt-0 mt-10">
-      <div className="w-full flex flex-col">
-        <div className="flex flex-row justify-between items-center">
-          <h2 className="text-blue-700 font-bold text-2xl">Top Charts</h2>
-          <Link to="/top-charts"><p className="text-blue-700 text-base cursor-pointer">See more</p></Link>
-        </div>
-        <div className="mt-4 flex flex-col gap-1">
-          {topPlays?.map((song, i) => (
-            <TopChartCard
-              key={song.key}
-              song={song}
-              i={i}
-              isPlaying={isPlaying}
-              activeSong={activeSong}
-              handlePauseClick={handlePauseClick}
-              handlePlayClick={() => handlePlayClick(song, i)}
-            />
-          ))}
-        </div>
-      </div>
-
       <div className="w-full flex flex-col mt-8">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-blue-700 font-bold text-2xl">Top Artists</h2>
@@ -104,6 +84,26 @@ const TopPlay = () => {
             </SwiperSlide>
           ))}
         </Swiper>
+      </div>
+
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-blue-700 font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts"><p className="text-blue-700 text-base cursor-pointer">See more</p></Link>
+        </div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
